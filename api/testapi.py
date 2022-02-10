bcb47c80cd4a1c4ee8c099dc173576d7a6433813
Đạt Nguyễn <datnt@segu.vn>
2022-02-10 16:47:09 +0700

    update tool

--- api/testapi.py
+++ api/testapi.py
@@ -1,8 +1,11 @@
 import socket
+import requests
 from flask import Flask
 
 app = Flask(__name__)
 app.config["DEBUG"] = False
+
+R_URL = 'https://muse.theartica.app/type/'
 
 
 @app.route('/')
@@ -20,6 +23,36 @@ def arAPI():
 def homeAPI():
     f = open('home_content.json', encoding="utf8")
     return str(f.read())
+
+
+@app.route('/doodle_pens', methods=['GET'])
+def doodle_pensAPI():
+    response = requests.get(R_URL + 'doodle_pens')
+    return str(response.json())
+
+
+@app.route('/filters', methods=['GET'])
+def filtersAPI():
+    response = requests.get(R_URL + 'filters')
+    return str(response.json())
+
+
+@app.route('/makeup_tools', methods=['GET'])
+def makeup_toolsAPI():
+    response = requests.get(R_URL + 'makeup_tools')
+    return str(response.json())
+
+
+@app.route('/model_styles', methods=['GET'])
+def model_stylesAPI():
+    response = requests.get(R_URL + 'model_styles')
+    return str(response.json())
+
+
+@app.route('/stickers', methods=['GET'])
+def stickersAPI():
+    response = requests.get(R_URL + 'stickers')
+    return str(response.json())
 
 
 if __name__ == '__main__':
