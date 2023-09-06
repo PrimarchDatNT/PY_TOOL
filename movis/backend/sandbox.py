3ff51bd48d4dabf1544f556d4eb6eac1978fafa2
DatNT <datnt@segu.vn>
2023-09-07 00:16:50 +0700

    [Change fx data]

--- movis/backend/sandbox.py
+++ movis/backend/sandbox.py
@@ -108,7 +108,7 @@ def change_font_imgs():
         for data in jsondata['data']:
             old_url = data['iconFromTemplate']
             new_url = str(data['iconFromTemplate']).replace('https://d2hyio9ps90xn9.cloudfront.net/videodata/',
-                                                            'https://d3pldjsx7tl7ei.cloudfront.net/font_imgs/videodata/')
+                                                            'https://d3pldjsx7tl7ei.cloudfront.net/fx_imgs/videodata/')
             print(old_url)
             print(new_url)
             data['iconFromTemplate'] = new_url
@@ -118,7 +118,29 @@ def change_font_imgs():
 
 
 def change_fx_imgs():
-    pass
+    for e in LIST_FX_URL:
+        print(e)
+    files = []
+    for p in LIST_JSON_FILE:
+        if p.__contains__('fx'):
+            files.append(p)
+            print(p)
+
+    for p in files:
+        print(p)
+        with open(p) as jsonfile:
+            jsondata = json.load(jsonfile)
+
+        for data in jsondata['data']:
+            old_url = data['iconFromTemplate']
+            new_url = str(old_url).replace('https://d2hyio9ps90xn9.cloudfront.net/videodata/',
+                                           'https://d3pldjsx7tl7ei.cloudfront.net/fx_imgs/videodata/')
+            print(old_url)
+            print(new_url)
+            data['iconFromTemplate'] = new_url
+            data['previewurl'] = new_url
+        with open(p, 'w', encoding='utf-8') as outjson:
+            json.dump(jsondata, outjson)
 
 
 def change_sticker_imgs():
@@ -178,4 +200,5 @@ if __name__ == '__main__':
     # change_background_imgs()
     # change_effect_imgs()
     # change_filter_imgs()
-    change_font_imgs()
+    # change_font_imgs()
+    change_fx_imgs()
