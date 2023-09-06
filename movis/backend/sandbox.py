35fbb2a154f3602eb7b3ffdd3a18f376b084825f
DatNT <datnt@segu.vn>
2023-09-06 23:43:36 +0700

    [Change background data]

--- movis/backend/sandbox.py
+++ movis/backend/sandbox.py
@@ -1,24 +1,122 @@
-import datamapper as mapper
 import os.path
 import json
 
-groupcode_arg = ('202012031942596710,202101251535376626,202007101833288799,201911181234523245,201912171631384790,'
-                 '201911181234523214')
-result_dict = {'code': 200, 'count': 0, 'data': []}
-count_result = 0
-data_detail = []
+CATE_DIR = 'E:/WORK/PY_PROJECT/TOOL/movis/backend/resource/detail'
+BUCKET_DOMAIN = 'https://d3pldjsx7tl7ei.cloudfront.net/'
+MOVIS_RES_DIR = 'C:/Users/DatNT/Desktop/appRes/MOVIS-RES/image-api-movis/'
+LIST_JSON_FILE = []
+LIST_REOUSOUCE_FILE = []
 
-for grc in groupcode_arg.split(','):
-    if mapper.source_detail_dict.__contains__(grc):
-        detail_file = 'resource/' + mapper.source_detail_dict[grc]
-        if os.path.exists(detail_file):
-            with open(detail_file, encoding="utf8", mode='r') as file:
-                print('Getting detail resource')
-                jsondata = json.load(file)
-                count_result += jsondata['count']
-                for d in jsondata['data']:
-                    data_detail.append(d)
-result_dict['count'] = count_result
-result_dict['data'] = data_detail
-json.dumps(result_dict)
-print(json.dumps(result_dict))
+LIST_BACKGROUND_URL = []
+LIST_EFFECT_URL = []
+LIST_FILTER_URL = []
+LIST_FONT_URL = []
+LIST_FX_URL = []
+LIST_STICKER_URL = []
+LIST_THEME_URL = []
+LIST_TRANSITION__URL = []
+
+
+def change_background_imgs():
+    mapper = {}
+    files = []
+    for p in LIST_JSON_FILE:
+        if p.__contains__('background'):
+            key = p[p.rindex('/'):].replace('/', '').replace('.json', '').lower()
+            mapper[key] = []
+            files.append(p)
+
+    for p in files:
+        print(p)
+        with open(p) as jsonfile:
+            data = json.load(jsonfile)
+
+        for entry in data['data']:
+            old_url = entry['downUrl']
+            if p.__contains__('Raw'):
+                url = str(old_url).replace('https://d2hyio9ps90xn9.cloudfront.net/videodata/background/background/',
+                                                    'https://d3pldjsx7tl7ei.cloudfront.net/background_imgs/videodata/background/raw/')
+            else:
+                url = str(old_url).replace('https://d2hyio9ps90xn9.cloudfront.net/videodata/background/',
+                                                    'https://d3pldjsx7tl7ei.cloudfront.net/background_imgs/videodata/background/')
+
+            entry['downUrl'] = url
+            entry['iconFromTemplate'] = url
+            entry['showImg'] = url
+
+        with open(p, 'w', encoding='utf-8') as outjson:
+            json.dump(data, outjson)
+
+
+def change_effect_imgs():
+    pass
+
+
+def change_filter_imgs():
+    pass
+
+
+def change_font_imgs():
+    pass
+
+
+def change_fx_imgs():
+    pass
+
+
+def change_sticker_imgs():
+    pass
+
+
+def change_theme_imgs():
+    pass
+
+
+def change_transition_imgs():
+    pass
+
+
+if __name__ == '__main__':
+
+    for r, d, f in os.walk(CATE_DIR):
+        for file in f:
+            if '.json' in file:
+                file_path = str(r + '/' + file).replace('\\', '/')
+                if file_path.__contains__('audio'):
+                    pass
+                else:
+                    LIST_JSON_FILE.append(file_path)
+
+    for r, d, f in os.walk(MOVIS_RES_DIR):
+        for file in f:
+            file_path = str(r + '/' + file).replace('\\', '/')
+            LIST_REOUSOUCE_FILE.append(file_path)
+
+    # for file in LIST_JSON_FILE:
+    #     if file.__contains__('audio/'):
+    #         pass
+    #     else:
+    #         cate = file[file.rindex('movis'):].replace('movis/backend/resource/detail/', '').replace('.json', '')
+    #         print(cate.lower())
+
+    for file in LIST_REOUSOUCE_FILE:
+        path = file.replace(MOVIS_RES_DIR, BUCKET_DOMAIN)
+        if path.__contains__('background_imgs'):
+            LIST_BACKGROUND_URL.append(path)
+        if path.__contains__('effect_imgs'):
+            LIST_EFFECT_URL.append(path)
+        if path.__contains__('filter_imgs'):
+            LIST_FILTER_URL.append(path)
+        if path.__contains__('font_imgs'):
+            LIST_FONT_URL.append(path)
+        if path.__contains__('fx_imgs'):
+            LIST_FX_URL.append(path)
+        if path.__contains__('sticker_imgs'):
+            LIST_STICKER_URL.append(path)
+        if path.__contains__('theme_imgs'):
+            LIST_THEME_URL.append(path)
+        if path.__contains__('transition_imgs'):
+            LIST_TRANSITION__URL.append(path)
+
+
+    change_background_imgs()
